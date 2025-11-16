7c17b79e2b20d110e18a4890567200cf16cc4647
James Wu <jameswu144@gmail.com>
2025-11-16 19:27:33 +0000

    add config

---
+++ neural_network/config.py
@@ -0,0 +1,17 @@
+
+class ModelConfig:
+    def __init__(self, input_size, hidden_sizes, output_size, activation, learning_rate):
+        self.input_size = input_size
+        self.hidden_sizes = hidden_sizes
+        self.output_size = output_size
+        self.activation = activation # needs to be a torch.nn activation function
+        self.learning_rate = learning_rate
+
+class MarketParams:
+    def __init__(self, r, sigma, K, T, S_min, S_max):
+        self.r = r
+        self.sigma = sigma
+        self.K = K
+        self.T = T
+        self.S_min = S_min
+        self.S_max = S_max
--- neural_network/trainer.py
+++ neural_network/trainer.py
@@ -6,38 +6,30 @@ import matplotlib.pyplot as plt
 from model import BaseNetwork
 from sampler import Sampler
 from losses import compute_derivatives, pde_residual
-
-
-class Parameters:
-    def __init__(self, r, sigma, K, T):
-        self.r = r
-        self.sigma = sigma
-        self.K = K
-        self.T = T
+from config import MarketParams, ModelConfig
 
 
 class NeuralNetworkTrainer:
-    def __init__(self, params, payoff, seed):
-        self.params = params
+    def __init__(self, model_config, market_params, payoff, seed):
+        self.model_config = model_config
+        self.market_params = market_params
         self.payoff = payoff
-
         self.set_seed(seed)
 
-        self.act_fn = nn.ReLU()
         self.model = BaseNetwork(
-            act_fn=self.act_fn,
-            input_size=2,
-            output_size=1,
-            hidden_sizes=[32, 32, 32]
+            act_fn=model_config.activation,
+            input_size=model_config.input_size,
+            output_size=model_config.output_size,
+            hidden_sizes=model_config.hidden_sizes
         )
 
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=model_config.learning_rate)
 
         self.sampler = Sampler(
             t_min=0.0,
-            t_max=1.0,
-            S_min=0.0,
-            S_max=2.0
+            t_max=market_params.T,
+            S_min=market_params.S_min,
+            S_max=market_params.S_max
         )
 
         self.history = {
@@ -48,12 +40,12 @@ class NeuralNetworkTrainer:
         np.random.seed(seed)
         torch.manual_seed(seed)
 
-    def train(self, max_iterations):
+    def train(self, num_samples, max_iterations):
         for i in range(max_iterations):
             self.optimizer.zero_grad()
 
             # Sample interior points
-            t_interior, S_interior = self.sampler.generate(N=1000)
+            t_interior, S_interior = self.sampler.generate(N=num_samples)
 
             # Compute derivatives
             v, v_t, v_S, v_SS = compute_derivatives(self.model, t_interior, S_interior)
@@ -62,19 +54,18 @@ class NeuralNetworkTrainer:
             residual = pde_residual(
                 v_t, v_S, v_SS, v,
                 S_interior,
-                self.params.r,
-                self.params.sigma
+                self.market_params.r,
+                self.market_params.sigma
             )
-            pde_loss = torch.min(residual, v - self.payoff(S_interior, self.params.K))
+            pde_loss = torch.min(residual, v - self.payoff(S_interior, self.market_params.K))
             pde_loss = torch.mean(pde_loss**2)
 
             # Boundary conditions
-            num_samples = 1000
             t_b, S_b = self.sampler.generate(num_samples)
             ones = torch.ones(num_samples, 1)
 
             v_b = self.model(ones, S_b)
-            payoff = self.payoff(S_b, self.params.K)
+            payoff = self.payoff(S_b, self.market_params.K)
             boundary_loss = nn.MSELoss()(v_b, payoff)
 
             # f(t, S_max) = 0
