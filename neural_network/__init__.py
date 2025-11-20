de9556406c8ed577bbd1c87b51021da993be4e15
James Wu <jameswu144@gmail.com>
2025-11-20 14:46:41 +0000

    make into library

---
+++ neural_network/__init__.py
@@ -0,0 +1,5 @@
+from .config import ModelConfig, MarketParams
+from .model import BaseNetwork
+from .sampler import Sampler
+from .trainer import NeuralNetworkTrainer
+from .losses import compute_derivatives, pde_residual
