5cd2c83fc661d13bafba7126f478587b3299064c
James Wu <jameswu144@gmail.com>
2026-03-29 13:00:00 +0100

    minor edit

--- utility/continuation_prob.py
+++ utility/continuation_prob.py
@@ -8,20 +8,19 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def p_cont(d, tau1, tau2):
-    tau = np.where(d < 0, tau1, tau2)
+def p_cont(d, tau):
     return sigmoid(d / tau)
 
 
-def compute_continuation_probs(prices, intrinsics, eps1, eps2, one=0.99, shift=0):
+def compute_continuation_probs(prices, intrinsics, eps, one=0.99, shift=0):
     """
     Compute continuation probabilities for a set of prices and intrinsic values. These are performed on the
     relative difference between them.
 
     Prices and intrinsics must be numpy arrays of the same shape.
 
-    We have eps1 and eps2 as parameters that control the shape of the continuation probability function, such that
-    sigmoid(-eps1 / tau1) = 1 - one and sigmoid(eps2 / tau2) = one
+    We have eps as a parameter that controls the shape of the continuation probability function, such that
+    sigmoid(-eps / tau) = 1 - one and sigmoid(eps / tau) = one
 
     `shift` is added to prices to adjust the probabilities, defaulted to 0. This is useful for tree / closed form
     prices where we necessarily have price >= intrinsic, for neural networks this may not be the case
@@ -30,12 +29,11 @@ def compute_continuation_probs(prices, intrinsics, eps1, eps2, one=0.99, shift=0
     assert prices.shape == intrinsics.shape, "Prices and intrinsics must have the same shape."
     assert 0.5 < one < 1, "one must be between 0.5 and 1."
 
-    tau1 = eps1 / np.log(one / (1 - one))
-    tau2 = eps2 / np.log(one / (1 - one))
+    tau = eps / np.log(one / (1 - one))
 
     d = (prices - intrinsics + shift) / intrinsics
 
-    cont_probs = p_cont(d, tau1, tau2)
+    cont_probs = p_cont(d, tau)
     cont_probs = np.where(intrinsics <= 0, 1.0, cont_probs)  # If the option is out of the money, we always continue
 
     return cont_probs
@@ -68,11 +66,11 @@ def estimate_continuation_value(model, t, S, r, sigma, n_paths=100, h=0.01, seed
     return cont_value
 
 
-def plot_p_cont(tau1, tau2):
+def plot_p_cont(tau):
     x = np.linspace(-4, 4, 500)
-    y = p_cont(x, tau1, tau2)
+    y = p_cont(x, tau)
     plt.plot(x, y)
-    plt.title(f'p_cont with tau1={tau1} and tau2={tau2}')
+    plt.title(f'p_cont with tau={tau}')
     plt.xlabel('x')
     plt.ylabel('p_cont(x)')
     plt.grid()
@@ -80,6 +78,5 @@ def plot_p_cont(tau1, tau2):
 
 
 if __name__ == "__main__":
-    tau1 = 0.1
-    tau2 = 1
-    plot_p_cont(tau1, tau2)
+    tau = 0.1
+    plot_p_cont(tau)
